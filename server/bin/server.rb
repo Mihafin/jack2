eea670c19d2b2efeb47e09e62f71a88de5f5a4a9
Mihail Finogeev <mihail.fin@gmail.com>
2012-10-29 18:00:44 +0400

    fx
    iml rem
    
    rm
    
    fx

--- server/bin/server.rb
+++ server/bin/server.rb
@@ -8,13 +8,16 @@ get '/' do
 end
 
 get '/bin/app.swf' do
+  content_type 'application/x-shockwave-flash'
   File.read("../client/bin/jack2.swf")
 end
 
 get '/js/swfobject.js' do
+  content_type 'text/javascript'
   File.read("../client/html/js/swfobject.js")
 end
 
 get '/css/common.css' do
+  content_type 'text/css'
   File.read("../client/html/css/common.css")
 end
